8db0e91b89b5030078c04d460644b8363709f4ef
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-03-25 11:29:33 +0900

    B. V

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import sys
 total_num, mark_num = map(int, input().split())
+
+if mark_num == 0:
+  ans = list(range(1,total_num+1))
+  print(*ans)
+  sys.exit()
+
 mark_place_list = list(map(int, input().split()))
 
 result = []
 temp_list = []
 pre_num = 0
+count = 0
 for num in mark_place_list:
+    count += 1
+
     if num == pre_num + 1:
         temp_list.append(num)
     else:
@@ -12,10 +22,22 @@ for num in mark_place_list:
         result.extend(reversed(temp_list))
         temp_list = []
         if num - pre_num > 2:
-            result.extend(list(range[pre_num+2:num-1]))            
-        else:
-            temp_list.append(num)
+            result.extend(list(range(pre_num+2,num)))            
+        
+        temp_list.append(num)
+
+    if count == mark_num:
+        temp_list.append(num + 1)
+
     pre_num = num
 
 result.extend(reversed(temp_list))
-print(" ".join(result))    
+
+last_num = mark_place_list[mark_num-1]
+additional_num = total_num - last_num
+
+if  additional_num > 1:
+    left_list = list(range(last_num+2,total_num+1))
+    result.extend(left_list)
+
+print(" ".join(map(str,result)))    
