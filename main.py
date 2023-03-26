353f46491794e09748ed5e1e438fd7c7767f6b65
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-03-27 08:14:17 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,43 +1,11 @@
-import sys
-total_num, mark_num = map(int, input().split())
+all_num, tgt_num = map(int, input().split())
 
-if mark_num == 0:
-  ans = list(range(1,total_num+1))
-  print(*ans)
-  sys.exit()
-
-mark_place_list = list(map(int, input().split()))
-
-result = []
-temp_list = []
-pre_num = 0
-count = 0
-for num in mark_place_list:
-    count += 1
-
-    if num == pre_num + 1:
-        temp_list.append(num)
+ranker_list = []
+for index, _ in enumerate(range(all_num), 1):
+    if index > tgt_num:
+        input()
     else:
-        temp_list.append(pre_num + 1)
-        result.extend(reversed(temp_list))
-        temp_list = []
-        if num - pre_num > 2:
-            result.extend(list(range(pre_num+2,num)))            
-        
-        temp_list.append(num)
+        ranker_list.append(input())
 
-    if count == mark_num:
-        temp_list.append(num + 1)
-
-    pre_num = num
-
-result.extend(reversed(temp_list))
-
-last_num = mark_place_list[mark_num-1]
-additional_num = total_num - last_num
-
-if  additional_num > 1:
-    left_list = list(range(last_num+2,total_num+1))
-    result.extend(left_list)
-
-print(" ".join(map(str,result)))    
+for name in sorted(ranker_list):
+    print(name)
