09da391338944cbde97781abd8606e6324331536
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-03-30 07:49:42 +0900

    B-Cat

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-N, M = map(int, input().split())
-s_list = [ input()[3:] for _ in range(N)]
-t_list = [ input() for _ in range(M)]
+N = int(input())
+S = input()
 
-s_list = sorted(s_list)
-t_list = sorted(t_list)
-
-count = 0
-for s in s_list:
-    for t in t_list:
-        if s == t :
-            count += 1
-            break
-
-print(count)
+print(S.replace('na', 'nya'))
