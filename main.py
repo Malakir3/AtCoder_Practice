81b375f622aae2c725c6638c57754c2451efb20e
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-05 08:16:58 +0900

    C - multiple list

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-N = int(input())
-a_list = list(map(int, input().split()))
-Q = int(input())
+N, X = map(int, input().split())
+ls = list(map(int, input().split()))
 
-def my_query(arg_list):
-    if arg_list[0] == 1:
-        a_list[arg_list[1]-1] = arg_list[2]
-    else:
-        print(a_list[arg_list[1]-1])
+st = set(ls)
 
-for _ in range(Q):
-    q = list(map(int, input().split()))
-    my_query(q)
-        
+ans = 'No'
+for num in st:
+    if num + X in st:
+        ans = 'Yes'
+        break
+
+print(ans)
