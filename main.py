0894c6d7f760ef1960f0fc435e0d1b4d5764cf74
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-07 08:16:17 +0900

    C - Socks AC

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 N = int(input())
 ls = list(map(int, input().split()))
 
-st = set(ls)
+# st = set(ls)
 
+# count = 0
+# for color in st:
+#     count += ls.count(color) // 2
+
+# print(count)
+
+
+s_ls = sorted(ls)
+
+pre_color = 0
 count = 0
-for color in st:
-    count += ls.count(color) // 2
-
+for color in s_ls:
+    if pre_color == 0:
+        pre_color = color
+        continue
+    
+    if color == pre_color:
+        count += 1
+        pre_color = 0
+    else:
+        pre_color = color
+        
 print(count)
