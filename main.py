62ec1514d61ab940186ec040f5d7e801d971fc38
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-10 07:48:26 +0900

    C - Socks by defaultdict

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
+from collections import defaultdict
+
 N = int(input())
 ls = list(map(int, input().split()))
 
-# st = set(ls)
+dic = defaultdict(int)
 
-# count = 0
-# for color in st:
-#     count += ls.count(color) // 2
+for num in ls:
+    dic[num] += 1
 
-# print(count)
-
-
-s_ls = sorted(ls)
-
-pre_color = 0
 count = 0
-for color in s_ls:
-    if pre_color == 0:
-        pre_color = color
-        continue
-    
-    if color == pre_color:
-        count += 1
-        pre_color = 0
-    else:
-        pre_color = color
-        
+for key in dic:
+    count += dic[key] // 2
+
 print(count)
