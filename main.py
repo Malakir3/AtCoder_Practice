cc447e6fced3da389dd8e040e6c5e45a347db43f
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-13 08:17:08 +0900

    failed

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-# C - Make Takahashi Happy 
+# C - Make Takahashi Happy
+
+# 3 3
+# 3 2 2
+# 2 1 3
+# 1 5 4
+# [[3, 2, 2], [2, 1, 3], [1, 5, 4]]
+
+import itertools
+
+H, W = map(int, input().split())
+
+# [[3, 2, 2], [2, 1, 3], [1, 5, 4]]
+ls = [ list(map(int, input().split())) for _ in range(H)]
+
+move_num_ls = [ _ for _ in range(H+W-2)] # [0,1,2,...,H+W-1]
+
+count = 0
+for down_time in itertools.combinations(move_num_ls, H-1): # (0,2)等
+    trail_log = set()
+    trail_log.add(ls[0][0])
+    
+    x = 0
+    y = 0
+    for time in range(H+W-2):
+        if time == down_time:
+            y += 1
+        else:
+            x += 1
+        # trail_log.add(ls[y][x])
+        print(trail_log)
+    
+    if len(trail_log) == H+W-1:
+        count += 1
+
+print(count)
+
+    
