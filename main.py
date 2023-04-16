9c3a19b816cb3b2f9282a2d1f4fe87eaeeee258b
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-17 08:04:43 +0900

    # C - LRUD Instructions 2

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
-# C - Four Variables
+# C - LRUD Instructions 2 
+from collections import defaultdict
 
 n = int(input())
+s_ls = [ _ for _ in input()]
 
-# 1 <= ab <= n-1を満たすa,bの組み合わせ（約数の数）を調べる。
-# 一方が決まれば他方も決まる。
-
-# その後、cd = n-abを満たすc,dの組み合わせを調べる。
-
-# resultに、abとcdの組み合わせ方の数を加える。
-# a != b かつ、c !=d のときは+4
-# a = b かつ、c = d のときは+1
-# 他は+2
-# dic = map()
-ls = list()
-for ab in range(1,n):
-    # count = 0
-    i = 1
-    while i**2 <= ab:
-        if ab % i == 0:
-            a = i
-            b = ab // i
-            cd = n - ab
-
-            j = 1
-            while j**2 <= cd:
-                if cd % j == 0:
-                    c = j
-                    d = cd // j
-                ls.append([a,b,c,d])
-                j += 1
-        i += 1
-
-count = 0
-for pair in ls:
-    a = pair[0]
-    b = pair[1]
-    c = pair[2]
-    d = pair[3]
-    if a != b & c != d:
-        count += 4
-    elif len(set(pair)) == 1:
-        count += 1
+dic = defaultdict(set)
+x = 0
+y = 0
+dic[(0,0)] = 0
+for str in s_ls:
+    if str == "R":
+        x += 1
+    elif str == "L":
+        x -= 1
+    elif str == "U":
+        y += 1
     else:
-        count += 2
+        y -= 1
 
-print(count)        
+    dic[(x,y)] = 0
 
+if len(dic) != n+1:
+    print('Yes')
+else:
+    print('No')
