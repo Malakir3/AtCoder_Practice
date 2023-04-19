2d57046506e4d0b7587a34f00e8a89a323dd42fd
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-20 08:12:54 +0900

    Fail

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ a_set = sorted(set(a_ls))
 if a_set[0] != 0:
     print(0)
     sys.exit()
+elif len(a_set) == k:
+    if k == 1:
+        print(1)
+    else:
+        print(k)
+    sys.exit()
 
 pre_num = -1
 for index, i in enumerate(a_set):
     result = pre_num + 1
 
-    if i == pre_num + 1:
+    if i == result:
         pre_num = i
     else:
         break
