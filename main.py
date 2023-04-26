547a5c45a19f14e93871747c1d5bd29be6dd37e4
Malakir3 <79259080+Malakir3@users.noreply.github.com>
2023-04-27 08:15:47 +0900

    Add find, unite

--- main.py
+++ main.py
@@ -8,117 +8,95 @@ class UnionFind():
         self.root = [-1]*(n+1)
         self.rank = [0]*(n+1)
 
-    # def find(self, x):
-    #     """
-    #     ノードxの根を見つける
+    def find(self,x):
+        if(self.root[x] < 0):
+            return x
+        else:
+            self.root[x] = self.find(self.root[x])
+            return self.root[x]
+        
+    def unite(self,x,y):
+        x = self.find(x)
+        y = self.find(y)
 
-    #     Parameters
-    #     ---------------------
-    #     x : int
-    #         見つけるノード
+        if(x == y):
+            return
+        elif(self.rank[x] > self.rank[y]):
+            self.root[x] += self.root[y]
+            self.root[y] = x
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            if(self.rank[x] == self.rank[y]):
+                self.rank[y] += 1
+            
 
-    #     Returns
-    #     ---------------------
-    #     root : int
-    #         根のノード
-    #     """
-    #     if(self.root[x] < 0):
-    #         return x
-    #     else:
-    #         self.root[x] = self.find(self.root[x])
-    #         return self.root[x]
+  #  def same(self, x, y):
+  #       """
+  #       同じグループに属するか判定
 
-    # def unite(self, x, y):
-    #     """
-    #     木の併合
+  #       Parameters
+  #       ---------------------
+  #       x : int
+  #           判定したノード
+  #       y : int
+  #           判定したノード
 
-    #     Parameters
-    #     ---------------------
-    #     x : int
-    #         併合したノード
-    #     y : int
-    #         併合したノード
-    #     """
-    #     x = self.find(x)
-    #     y = self.find(y)
+  #       Returns
+  #       ---------------------
+  #       ans : bool
+  #           同じグループに属しているか
+  #       """
+  #       return self.find(x) == self.find(y)
 
-    #     if(x == y):
-    #         return
-    #     elif(self.rank[x] > self.rank[y]):
-    #         self.root[x] += self.root[y]
-    #         self.root[y] = x
-    #     else:
-    #         self.root[y] += self.root[x]
-    #         self.root[x] = y
-    #         if(self.rank[x] == self.rank[y]):
-    #             self.rank[y] += 1
+  #   def size(self, x):
+  #       """
+  #       木のサイズを計算
 
-    # def same(self, x, y):
-    #     """
-    #     同じグループに属するか判定
+  #       Parameters
+  #       ---------------------
+  #       x : int
+  #           計算したい木のノード
 
-    #     Parameters
-    #     ---------------------
-    #     x : int
-    #         判定したノード
-    #     y : int
-    #         判定したノード
+  #       Returns
+  #       ---------------------
+  #       size : int
+  #           木のサイズ
+  #       """
+  #       return -self.root[self.find(x)]
 
-    #     Returns
-    #     ---------------------
-    #     ans : bool
-    #         同じグループに属しているか
-    #     """
-    #     return self.find(x) == self.find(y)
+  #   def roots(self):
+  #       """
+  #       根のノードを取得
 
-    # def size(self, x):
-    #     """
-    #     木のサイズを計算
+  #       Returns
+  #       ---------------------
+  #       roots : list
+  #           根のノード
+  #       """
+  #       return [i for i, x in enumerate(self.root) if x < 0]
 
-    #     Parameters
-    #     ---------------------
-    #     x : int
-    #         計算したい木のノード
+  #   def group_size(self):
+  #       """
+  #       グループ数を取得
 
-    #     Returns
-    #     ---------------------
-    #     size : int
-    #         木のサイズ
-    #     """
-    #     return -self.root[self.find(x)]
+  #       Returns
+  #       ---------------------
+  #       size : int
+  #           グループ数
+  #       """
+  #       return len(self.roots())
 
-    # def roots(self):
-    #     """
-    #     根のノードを取得
+  #   def group_members(self):
+  #       """
+  #       全てのグループごとのノードを取得
 
-    #     Returns
-    #     ---------------------
-    #     roots : list
-    #         根のノード
-    #     """
-    #     return [i for i, x in enumerate(self.root) if x < 0]
-
-    # def group_size(self):
-    #     """
-    #     グループ数を取得
-
-    #     Returns
-    #     ---------------------
-    #     size : int
-    #         グループ数
-    #     """
-    #     return len(self.roots())
-
-    # def group_members(self):
-    #     """
-    #     全てのグループごとのノードを取得
-
-    #     Returns
-    #     ---------------------
-    #     group_members : defaultdict
-    #         根をキーとしたノードのリスト
-    #     """
-    #     group_members = defaultdict(list)
-    #     for member in range(self.n):
-    #         group_members[self.find(member)].append(member)
-    #     return group_members
+  #       Returns
+  #       ---------------------
+  #       group_members : defaultdict
+  #           根をキーとしたノードのリスト
+  #       """
+  #       group_members = defaultdict(list)
+  #       for member in range(self.n):
+  #           group_members[self.find(member)].append(member)
+  #       return group_members
